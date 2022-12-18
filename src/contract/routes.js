0da1f6453e6f8ec50a27e29f793a11cca845fa40
Ismael Costa <ismael.costa@clicksign.com>
2022-12-18 18:30:22 -0300

    feat: updating the contract routes

--- src/contract/routes.js
+++ src/contract/routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const ContractController = require('./controller')
 
-router.post('/contract', ContractController.create)
-router.get('/contract', ContractController.get)
-router.get('/contract/:id', ContractController.getById)
-router.patch('/contract/:id', ContractController.update)
-router.delete('/contract/:id', ContractController.delete)
+router.post('/', ContractController.create)
+router.get('/', ContractController.get)
+router.get('/:id', ContractController.getById)
+router.patch('/:id', ContractController.update)
+router.delete('/:id', ContractController.delete)
 
 module.exports = router
